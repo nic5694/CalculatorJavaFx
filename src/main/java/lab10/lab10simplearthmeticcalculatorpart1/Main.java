afad6871412908fa2a887d580e3d5a5d40603003
Nic <101201789+nic5694@users.noreply.github.com>
2022-05-05 11:54:28 -0400

    adding filed to version control

--- src/main/java/lab10/lab10simplearthmeticcalculatorpart1/Main.java
+++ src/main/java/lab10/lab10simplearthmeticcalculatorpart1/Main.java
@@ -1,13 +1,38 @@
 package lab10.lab10simplearthmeticcalculatorpart1;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.IOException;
+
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        stage.setTitle("Calculator");
+        stage.setMinWidth(800);
+        stage.setMinHeight(800);
+        stage.setResizable(false);
+        stage.centerOnScreen();
 
+
+        HBox hBox = new HBox();
+        ListView listView = new ListView();
+        VBox vBox = new VBox();
+        GridPane gridPane = new GridPane();
+        Label label = new Label();
+        hBox.getChildren().addAll(listView, vBox);
+        vBox.getChildren().addAll(label, gridPane);
+        Scene  scene = new Scene(hBox);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module lab10.lab10simplearthmeticcalculatorpart1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.kordamp.bootstrapfx.core;
 
 
     opens lab10.lab10simplearthmeticcalculatorpart1 to javafx.fxml;
